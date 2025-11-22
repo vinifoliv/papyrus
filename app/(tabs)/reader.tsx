3a0c48997684f5c65ed62732e0cf16b2255a6b57
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2025-11-22 00:05:18 -0300

    feat: render PDF

--- app/(tabs)/reader.tsx
+++ app/(tabs)/reader.tsx
@@ -1,16 +1,44 @@
 import ScreenTitle from "@/components/ScreenTitle";
-import { useTheme } from "react-native-paper";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemedAreaView } from "@/components/ThemedAreaView";
+import { useDocumentPicker } from "@/hooks/useDocumentPicker";
+import { File } from "@/interfaces/File";
+import { useState } from "react";
+import { Dimensions, View } from "react-native";
+import { ActivityIndicator, useTheme, Text } from "react-native-paper";
+import Pdf from "react-native-pdf";
+
+const { width, height } = Dimensions.get("window");
 
 export default function Reader() {
   const theme = useTheme();
+  const { selectFile } = useDocumentPicker();
+
+  const [loading, setLoading] = useState(false);
+  const [pages, setPages] = useState(0);
+
+  const [selectedFile, setSelectedFile] = useState<File | null>();
+
+  const handleSelectDocument = async () => {
+    const file = await selectFile();
+    if (file) setSelectedFile(file);
+  };
+
+  if (!selectedFile) (async () => await handleSelectDocument())();
 
   return (
-    <SafeAreaView
-      className="flex-1"
-      style={{ backgroundColor: theme.colors.background }}
-    >
-      <ScreenTitle>Reader</ScreenTitle>
-    </SafeAreaView>
+    <ThemedAreaView>
+      <View className="flex-1 p-5">
+        <ScreenTitle>Reader</ScreenTitle>
+        {loading && (
+          <View>
+            <ActivityIndicator size="large" color={theme.colors.primary} />
+            <Text variant="bodyMedium" style={{ marginTop: 10 }}>
+              Carregando PDF...
+            </Text>
+          </View>
+        )}
+        <Pdf style={{ flex: 1, width }} source={{ uri: selectedFile?.uri }} />
+      </View>
+    </ThemedAreaView>
   );
 }
